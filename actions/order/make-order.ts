a36f23b1d3fc45744438c9fc4dcd3e7fd1695db9
mguzman20 <m.gguzman@uc.cl>
2024-09-24 10:42:44 -0300

    feat: Eliminar lógica para verificar el stock de productos en check-sku.ts y agregar lógica para crear órdenes en make-order.ts

--- actions/order/make-order.ts
+++ actions/order/make-order.ts
@@ -2,6 +2,11 @@
 
 import connectDB from "@/lib/db"
 import Order from "@/models/Order"
+import Product from "@/models/Product"
+import { requestProducts } from "../product/request-products";
+import { getSpaces } from "../space/get-spaces";
+import { moveProduct } from "../product/move-product";
+import { getSpaceProducts } from "../space/get-space-products";
 
 export async function makeOrder(orderId: string) {
     try {
@@ -10,8 +15,33 @@ export async function makeOrder(orderId: string) {
         const order = await Order.findById(orderId);
 
         for (const product of order.products) {
+            const spaces = await getSpaces();
+            const productData = await Product.find({ sku: product.sku });
+
+            // check if the product is available in kitchen
+            // check if ingredients are available in kitchen
+            if (spaces && spaces.kitchen && spaces.kitchen.skuCount[product.sku] > product.quantity) {
+                // move to check-out area each one of the products
+                // get product id, remember to order them by expire date
+                const kitchenProduct = await getSpaceProducts(spaces.kitchen.id, product.sku);
+
+                for (let i = 0; i < product.quantity; i++) {
+                    await moveProduct(spaces.checkOut.id, kitchenProduct[i].id);
+                }
+            }
             
+            // move the product from the warehouse to the kitchen
+            // if error, request more products and wait for them to arrive
+
+            // request products from the warehouse
+
+            // when the product is available in the kitchen, move it to the check-out area
+
+            // check quantities and request more products if needed
         }
+
+        // deliver order
+
         
         return {
             status: 'Aceptado'
