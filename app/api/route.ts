b021bcfe4b9ac1604e5ab15781e91b4383942c8e
mguzman20 <m.gguzman@uc.cl>
2024-09-25 15:36:09 -0300

    feat: Agregar lógica para obtener productos en get-products.ts
    Agregar lógica para obtener productos en get-products.ts. Esto permitirá obtener la lista de productos disponibles para su posterior uso en el área de check-out.

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,11 +1,13 @@
 'use server'
 
+import { getProducts } from '@/actions/product/get-products';
 import { getProductCount } from '@/actions/space/get-product-count';
 import { getSpaces } from '@/actions/space/get-spaces';
 
 import { NextResponse } from 'next/server';
 
 export async function GET() {
+    const products = await getProducts();
     const spaces = await getSpaces();
     
 
