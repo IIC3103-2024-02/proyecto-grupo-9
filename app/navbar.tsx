dceb2ecaef883969276414b8299ab44035fb38fb
mguzman20 <m.gguzman@uc.cl>
2024-09-26 17:49:57 -0300

    Refactorizar estilos del navbar

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -7,10 +7,10 @@ export default function Navbar() {
     const pathname = usePathname();
 
     return (
-        <nav className= border-gray-200 bg-gray-900">
+        <nav className= "border-gray-200 bg-gray-900">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <div>
-                    <h1 className="text-2xl font-bold text-gray-900 dark:text-white">Proyecto grupo 9</h1>
+                    <h1 className="text-2xl font-bold text-white">Proyecto grupo 9</h1>
                 </div>
                 <div className="hidden w-full md:block md:w-auto" id="navbar-default">
                     <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
