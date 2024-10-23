64ce975d8d51181336e602620f5865ca499164aa
mguzman20 <m.gguzman@uc.cl>
2024-10-23 16:55:50 -0300

    stock up

--- actions/order/stock-up2.ts
+++ actions/order/stock-up2.ts
@@ -4,13 +4,13 @@ import { getSpaces } from "../space/get-spaces";
 import { requestProducts } from "../product/request-products";
 
 const products = [
-    { sku: 'CAFEGRANO', threshold: 10, quantity: 30 },
-    { sku: 'LECHEENTERA', threshold: 12, quantity: 24 },
-    { sku: 'AZUCARSACHET', threshold: 30, quantity: 100 },
-    { sku: 'ENDULZANTESACHET', threshold: 30, quantity: 100 },
-    { sku: 'VASOCAFE', threshold: 30, quantity: 100 },
-    { sku: 'VASOCAFEDOBLE', threshold: 30, quantity: 80 },
-    { sku: 'VASOCAFEEXPRESO', threshold: 30, quantity: 120 },
+    { sku: 'CAFEGRANO', threshold: 10, quantity: 30, ordered: false },
+    { sku: 'LECHEENTERA', threshold: 12, quantity: 24, ordered: false },
+    { sku: 'AZUCARSACHET', threshold: 30, quantity: 100, ordered: false },
+    { sku: 'ENDULZANTESACHET', threshold: 30, quantity: 100, ordered: false },
+    { sku: 'VASOCAFE', threshold: 30, quantity: 100, ordered: false },
+    { sku: 'VASOCAFEDOBLE', threshold: 30, quantity: 80, ordered: false },
+    { sku: 'VASOCAFEEXPRESO', threshold: 30, quantity: 120, ordered: false },
 ];
 
 async function stockUp() {
@@ -22,18 +22,20 @@ async function stockUp() {
         return;
     }
 
-    products.forEach(({ sku, threshold, quantity }) => {
-        const totalStock =
-            (spaces.checkIn.skuCount?.[sku] || 0) + (spaces.buffer.skuCount?.[sku] || 0);
+    products.forEach(({ sku, threshold, quantity, ordered }) => {
+        const totalStock = (spaces.checkIn.skuCount?.[sku] || 0) + (spaces.buffer.skuCount?.[sku] || 0);
 
-        if (totalStock < threshold) {
+        if (totalStock < threshold && !ordered) {
             requestProducts({ sku, quantity });
+            ordered = true;
+        } else if (totalStock >= threshold && ordered) {
+            ordered = false;
         }
     });
 }
 
 // Function to start the interval
-export function startStockCheckInterval(intervalMinutes = 30) {
+export function startStockCheckInterval(intervalMinutes = 20) {
     stockUp();
     const intervalMs = intervalMinutes * 60 * 1000;
     console.log(`Iniciando intervalo de verificación de stock cada ${intervalMinutes} minutos`);
