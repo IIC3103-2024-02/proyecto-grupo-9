65a0216f98b61601fbb14991352568443d56001f
mguzman20 <m.gguzman@uc.cl>
2024-11-17 19:35:20 -0300

    Deshabilitar la llamada a monitorDirectory en la instrumentación y en la gestión de errores de SFTP

--- instrumentation.ts
+++ instrumentation.ts
@@ -5,7 +5,7 @@ export async function register () {
         const { monitorDirectory } = await import("./lib/sftp");
         // const { startStockCheckInterval } = await import("./actions/order/stock-up");
         // const { getProducts } = await import("./actions/product/get-products");
-        monitorDirectory('/pedidos');
+        /* monitorDirectory('/pedidos'); */
         // startStockCheckInterval(20);
         // await getProducts();
     }
