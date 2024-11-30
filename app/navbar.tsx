5445fee0b4277975aae50c63f9c60c803029f10e
Rosario Frugone <rosariofrugone@MacBook-Pro-de-Andres-2.local>
2024-11-30 19:07:28 -0300

    navbar

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -23,6 +23,9 @@ export default function Navbar() {
                         <li>
                             <Link href="/web/form" className={`block py-2 px-3  ${pathname === '/web/form' ? 'text-blue-500' : 'text-white'} `} aria-current="page">Crear Orden</Link>
                         </li>
+                        <li>
+                            <Link href="/web/invoices" className={`block py-2 px-3  ${pathname === '/web/invoices' ? 'text-blue-500' : 'text-white'} `} aria-current="page">Facturas</Link>
+                        </li>
                                 
                     </ul>
                 </div>
